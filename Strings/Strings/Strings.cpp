5ec6c477444c67e6dfaaef5cc78c8f76d8a5bdea
ahmed ismail <87954455+ahmed777ismail@users.noreply.github.com>
2024-08-04 10:00:35 +0300

    Add files via upload
    Strings

---
+++ Strings/Strings/Strings.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	string s, t;
+	cin >> s >> t;
+	cout << s.length() << " " << t.length() << endl;
+	cout << s + t << endl;
+	char temp = s[0];
+	s[0] = t[0];
+	t[0] = temp;
+	cout << s << " " << t << endl;
+	return 0;
+}
+
