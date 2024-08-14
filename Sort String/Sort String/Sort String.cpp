60b6e07ca78ea357525d8d124f355790672fef20
ahmed ismail <87954455+ahmed777ismail@users.noreply.github.com>
2024-08-14 15:11:35 +0300

    Add files via upload
    Sort String

---
+++ Sort String/Sort String/Sort String.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int arr[26] = { 0 };
+	int size;
+	cin >> size;
+	string value;
+	cin >> value;
+
+
+	for (int i = 0; i < size; i++)
+	{
+		char c;
+		cin >> c;
+		arr[(int)c - 97]++;
+
+	}
+	for (int i = 0; i <= 25; i++)
+	{
+		while (arr[i] != 0)
+		{
+			cout << (char)(i + 97);
+			arr[i]--;
+		}
+	}
+
+	return 0;
+}
+
