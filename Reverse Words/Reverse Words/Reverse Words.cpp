e9d5ff29c920f71fa578d778fc74af6cc66e12a7
ahmed ismail <87954455+ahmed777ismail@users.noreply.github.com>
2024-08-16 01:29:18 +0300

    Add files via upload
    Reverse Words

---
+++ Reverse Words/Reverse Words/Reverse Words.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string value;
+	int flag = 0;
+	while (cin>> value)
+	{
+		reverse(value.begin(), value.end());
+		if (flag)
+		{
+			cout << " ";
+		}
+		flag = 1;
+		cout << value;
+	}
+}
+
